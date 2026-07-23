1f43827c01fdee85f6ec9a2b1950d79ea22a4339
somcpp <saubhagya.sharma3.14@gmail.com>
2026-07-23 11:42:16 +0530

    client: Build initial React UI and frontend architecture

---
+++ client/src/api/axios.js
@@ -0,0 +1,31 @@
+import axios from "axios";
+
+const api = axios.create({
+  baseURL: "http://localhost:8080/api", // The backend is on port 8080 based on server.js defaults
+  withCredentials: true, // Important for cookies
+});
+
+// While we use HTTP-Only cookies for the JWT, we'll keep this interceptor
+// around in case the frontend still relies on a localStorage token for whatever reason.
+api.interceptors.request.use((config) => {
+  const token = localStorage.getItem("token");
+  if (token) config.headers.Authorization = `Bearer ${token}`;
+  return config;
+});
+
+api.interceptors.response.use(
+  (res) => res,
+  (err) => {
+    if (err.response?.status === 401) {
+      const path = window.location.pathname;
+      if (path !== "/login" && path !== "/register" && path !== "/") {
+        localStorage.removeItem("token");
+        localStorage.removeItem("user");
+        window.location.href = "/login";
+      }
+    }
+    return Promise.reject(err);
+  }
+);
+
+export default api;
